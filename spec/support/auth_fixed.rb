4e38108b06bdde3788d4707ba052bb031730e789
Nagendra Dhanakeerthi <nagendra.dhanakeerthi@gmail.com>
2025-07-25 22:48:15 -0700

    Code cleanup

--- spec/support/auth_fixed.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Load the fixed auth implementation
-require_relative '../../lib/mcp_on_ruby/server/auth/oauth_fixed'
-require_relative '../../lib/mcp_on_ruby/server/auth_fixed'
